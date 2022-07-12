dd4a14bf76e2b0bfd44b5e86949fa90398b8e03c
Maxime-Lao <Maxime.laotonlop@gmail.com>
2022-07-12 20:39:48 +0200

    Wip

--- 404.php
+++ 404.php
@@ -11,23 +11,25 @@ get_header();
 ?>
 
 <main id="site-content">
+	<section class="error">
+		<div class="container">
+			<div class="section-inner thin error404-content">
 
-	<div class="section-inner thin error404-content">
+				<h1 class="entry-title"><?php _e( '404 Error.', 'esgi' ); ?></h1>
 
-		<h1 class="entry-title"><?php _e( '404 Error.', 'esgi' ); ?></h1>
+				<div class="intro-text"><p><?php _e( "The page you were looking for couldn't be found. <br> Maybe try a search?", 'esgi' ); ?></p></div><br>
 
-		<div class="intro-text"><p><?php _e( "The page you were looking for couldn't be found. Maybe try a search?", 'esgi' ); ?></p></div>
+				<?php
+				get_search_form(
+					array(
+						'aria_label' => __( '404 not found', 'esgi' ),
+					)
+				);
+				?>
 
-		<?php
-		get_search_form(
-			array(
-				'aria_label' => __( '404 not found', 'esgi' ),
-			)
-		);
-		?>
-
-	</div><!-- .section-inner -->
-
+			</div><!-- .section-inner -->
+		</div>
+	</section>
 </main><!-- #site-content -->
 
 <?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
