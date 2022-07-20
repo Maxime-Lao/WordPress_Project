957908b753aae6f01ea9f7684efa8c47d136c6f4
Maxime-Lao <Maxime.laotonlop@gmail.com>
2022-07-20 21:18:29 +0200

    Partners page completed

--- template-parts/partners.php
+++ template-parts/partners.php
@@ -6,33 +6,33 @@
 ?>
 
     <main>	
-        <div class="container">
-            <section class="error">
-                <h1 class="uwu"><?= $post->post_title ?></h1><br>
+        <section class="partners">
+            <div class="container">
                 <div class="footer-cta pt-5 pb-5">
-                    <div class="row">
-                        <div class="col-xl-2 col-md-2 mb-30">
+                <h1 class="uwu"><?= $post->post_title ?></h1><br>
+                    <div class="partners-flex">
+                        <div class="flex-item">
                             <img src="<?php echo wp_get_attachment_url(get_theme_mod('partners_section_first_image')) ?>">
                         </div>
-                        <div class="col-xl-2 col-md-2 mb-30">
+                        <div class="flex-item">
                             <img src="<?php echo wp_get_attachment_url(get_theme_mod('partners_section_second_image')) ?>">
                         </div>
-                        <div class="col-xl-2 col-md-2 mb-30">
+                        <div class="flex-item">
                             <img src="<?php echo wp_get_attachment_url(get_theme_mod('partners_section_third_image')) ?>">
                         </div>
-                        <div class="col-xl-2 col-md-2 mb-30">
+                        <div class="flex-item">
                             <img src="<?php echo wp_get_attachment_url(get_theme_mod('partners_section_fourth_image')) ?>">
                         </div>
-                        <div class="col-xl-2 col-md-2 mb-30">
+                        <div class="flex-item">
                             <img src="<?php echo wp_get_attachment_url(get_theme_mod('partners_section_fifth_image')) ?>">
                         </div>
-                        <div class="col-xl-2 col-md-2 mb-30">
+                        <div class="flex-item">
                             <img src="<?php echo wp_get_attachment_url(get_theme_mod('partners_section_sixth_image')) ?>">
                         </div>
                     </div>
                 </div>
-            </section>
-        </div>
+            </div>
+        </section>
 	</main>
     <script>
         document.getElementById("logo").classList.remove("white-svg");
